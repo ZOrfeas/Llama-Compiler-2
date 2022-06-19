f742bc43ed3af619acaedfdc20ac95a839322715
ZOrfeas <orfeas.zografos@gmail.com>
2022-06-20 01:08:19 +0300

    Include syntactic sugar library and use it

--- typesystem/typesystem.cpp
+++ typesystem/typesystem.cpp
@@ -1,39 +1,41 @@
 #include <type_traits>
 
 #include "./types.hpp"
+#include "../utils/match-as-is.hpp"
 
 namespace typesys {
     // possibly nice syntax for matching variants:
     // https://github.com/AVasK/vx/blob/main/vx.hpp
 
     std::string Type::to_string() const {
-        return std::visit(overloaded{
+        return type_variant | vx::match {
             []<BuiltinTypePtr T>(T const& t) -> std::string {
                 return type_enum_to_str(T::element_type::type_enum);
             },
             []<ComplexTypePtr T>(T const& t) {
                 return t->to_string();
-            },
-        }, type_variant);
+            }
+        };
     }
     std::ostream& operator<<(std::ostream& os, Type const& t) {
         return os << t.to_string();
     }
     const char* Type::get_type_enum_str() const {
-        return std::visit(overloaded{
+        return type_variant | vx::match {
             []<AnyTypePtr T>(T const& t) {
                 return type_enum_to_str(T::element_type::type_enum);
             }
-        }, type_variant);
+        };
     }
     bool Type::operator==(Type const& other) const {
-        return std::visit(overloaded{
+        using std::make_tuple;
+        return make_tuple(type_variant, other.type_variant) | vx::match {
             [&]<AnyTypePtr T>(T const& t1, T const& t2) {
                 // They are the same || their contents are equal
                 return t1 == t2 || *t1 == *t2;
             },
             [](auto&& t1, auto&& t2) { return false; }
-        }, type_variant, other.type_variant);
+        };
     }
     bool Type::operator!=(Type const& other) const {
         return !(*this == other);
