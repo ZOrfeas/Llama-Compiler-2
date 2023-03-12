5452864751486d94be9b601dcc948a017252b685
ZOrfeas <orfeas.zografos@gmail.com>
2023-03-12 23:03:27 +0200

    Remove previous implementation to allow existing stuff to compile

--- src/pass/sem/expr.rs
+++ src/pass/sem/expr.rs
@@ -6,42 +6,43 @@ use crate::parse::ast::expr::{
 use crate::parse::ast::expr::{BinopKind, ExprKind};
 
 pub fn sem_expr<'a, 'b>(expr: &'a Expr, sem_table: &'b mut SemTable<'a>) -> SemResult<&'b Type> {
-    let expr_type: Type = match &expr.kind {
-        ExprKind::UnitLiteral => Type::Unit,
-        ExprKind::IntLiteral(_) => Type::Int,
-        ExprKind::FloatLiteral(_) => Type::Float,
-        ExprKind::CharLiteral(_) => Type::Char,
-        ExprKind::StringLiteral(_) => Type::Array {
-            dim_cnt: 1,
-            inner: Box::new(Type::Char),
-        },
-        ExprKind::BoolLiteral(_) => Type::Bool,
-        ExprKind::Tuple(expr_vec) => Type::Tuple(
-            expr_vec
-                .iter()
-                .map(|expr| sem_expr(expr, sem_table).cloned())
-                .collect::<Result<Vec<_>, _>>()?,
-        ),
-        ExprKind::Unop(unop) => sem_unop(unop, sem_table)?,
-        ExprKind::Binop(binop) => sem_binop(binop, sem_table)?,
-        ExprKind::Call(call) => {
-            if call.args.len() == 0 {
-                sem_constant_call(call, sem_table)?
-            } else {
-                sem_func_call(call, sem_table)?
-            }
-        }
-        ExprKind::ConstrCall(call) => sem_constructor_call(call, sem_table)?,
-        ExprKind::ArrayAccess(array_access) => sem_array_access(array_access, sem_table)?,
-        ExprKind::Dim(dim) => sem_dim(dim, sem_table)?,
-        ExprKind::New(type_) => Type::Ref(Box::new(type_.clone())),
-        ExprKind::LetIn(let_in) => sem_letin(let_in, sem_table)?,
-        ExprKind::If(if_expr) => sem_if(if_expr, sem_table)?,
-        ExprKind::While(while_expr) => sem_while(while_expr, sem_table)?,
-        ExprKind::For(for_expr) => sem_for(for_expr, sem_table)?,
-        ExprKind::Match(match_expr) => sem_match(match_expr, sem_table)?,
-    };
-    Ok(sem_table.insert_type(expr, expr_type))
+    // let expr_type: Type = match &expr.kind {
+    //     ExprKind::UnitLiteral => Type::Unit,
+    //     ExprKind::IntLiteral(_) => Type::Int,
+    //     ExprKind::FloatLiteral(_) => Type::Float,
+    //     ExprKind::CharLiteral(_) => Type::Char,
+    //     ExprKind::StringLiteral(_) => Type::Array {
+    //         dim_cnt: 1,
+    //         inner: Box::new(Type::Char),
+    //     },
+    //     ExprKind::BoolLiteral(_) => Type::Bool,
+    //     ExprKind::Tuple(expr_vec) => Type::Tuple(
+    //         expr_vec
+    //             .iter()
+    //             .map(|expr| sem_expr(expr, sem_table).cloned())
+    //             .collect::<Result<Vec<_>, _>>()?,
+    //     ),
+    //     ExprKind::Unop(unop) => sem_unop(unop, sem_table)?,
+    //     ExprKind::Binop(binop) => sem_binop(binop, sem_table)?,
+    //     ExprKind::Call(call) => {
+    //         if call.args.len() == 0 {
+    //             sem_constant_call(call, sem_table)?
+    //         } else {
+    //             sem_func_call(call, sem_table)?
+    //         }
+    //     }
+    //     ExprKind::ConstrCall(call) => sem_constructor_call(call, sem_table)?,
+    //     ExprKind::ArrayAccess(array_access) => sem_array_access(array_access, sem_table)?,
+    //     ExprKind::Dim(dim) => sem_dim(dim, sem_table)?,
+    //     ExprKind::New(type_) => Type::Ref(Box::new(type_.clone())),
+    //     ExprKind::LetIn(let_in) => sem_letin(let_in, sem_table)?,
+    //     ExprKind::If(if_expr) => sem_if(if_expr, sem_table)?,
+    //     ExprKind::While(while_expr) => sem_while(while_expr, sem_table)?,
+    //     ExprKind::For(for_expr) => sem_for(for_expr, sem_table)?,
+    //     ExprKind::Match(match_expr) => sem_match(match_expr, sem_table)?,
+    // };
+    // Ok(sem_table.insert_type(expr, expr_type))
+    todo!()
 }
 
 fn sem_unop<'a>(unop: &'a Unop, sem_table: &mut SemTable<'a>) -> SemResult<Type> {
